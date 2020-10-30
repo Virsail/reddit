b2f228a9c76163911e02c5027e695663d0d746d3
virsail <virsaileric@gmail.com>
2020-10-30 12:17:08 +0300

    define reister url in urls.py

---
+++ amazon/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from . import views
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static 
+
+urlpatterns = [
+    url(r'^registerPage/$', views.registerPage, name='registerPage'),
+  
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
