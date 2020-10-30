813012acbfcca44e7864beb8746bd16ab67d60d7
virsail <virsaileric@gmail.com>
2020-10-30 18:02:21 +0300

    import upload care to models.py'

--- amazon/models.py
+++ amazon/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 from pyuploadcare.dj.models import ImageField
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
