de76a575c5a74b9072821bb61a1bc2a3495f4814
virsail <virsaileric@gmail.com>
2020-10-30 22:08:18 +0300

    fix the error in django ratings module

--- amazon/models.py
+++ amazon/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from pyuploadcare.dj.models import ImageField
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
-from djangoratings.fields import RatingField
+
 
 
 # Create your models here.
