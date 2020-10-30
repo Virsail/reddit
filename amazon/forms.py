598492cbafaf753b3e3fd8f5ef32ee21c8250f84
virsail <virsaileric@gmail.com>
2020-10-30 12:03:41 +0300

    import user creation form into forms.py

---
+++ amazon/forms.py
@@ -0,0 +1,6 @@
+from django.forms import ModelForm
+from .models import Event 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib .auth.models import User
+from django import forms
+from pyuploadcare.dj.forms import ImageField
