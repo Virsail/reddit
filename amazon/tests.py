44f3848c55544c35bfbffdfbf98d46a4db705768
virsail <virsaileric@gmail.com>
2020-10-31 00:31:08 +0300

    test class for user profile

--- amazon/tests.py
+++ amazon/tests.py
@@ -3,3 +3,17 @@ from .models import Projects, Profile
 from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='felixgargado', id=1,  password='mbocochilo')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
+
