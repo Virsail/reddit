d9fcc857905eb833ff5afc1318f935b01c4dfd69
virsail <virsaileric@gmail.com>
2020-10-30 12:07:14 +0300

    create sign up form in forms.py

--- amazon/forms.py
+++ amazon/forms.py
@@ -1,6 +1,14 @@
 from django.forms import ModelForm
-from .models import Event 
+#from .models import Event 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib .auth.models import User
 from django import forms
 from pyuploadcare.dj.forms import ImageField
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
