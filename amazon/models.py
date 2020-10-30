e129ec18ac6e7714d7c3f1d64c4fedd3661a465e
virsail <virsaileric@gmail.com>
2020-10-30 22:16:27 +0300

    add class profile in models.py

--- amazon/models.py
+++ amazon/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from pyuploadcare.dj.models import ImageField
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+from django.db.models import ObjectDoesNotExist
 
 
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    picture = models.ImageField(upload_to = 'profile_pics/', blank=True, default='profile_pics/default.jpg')
+
