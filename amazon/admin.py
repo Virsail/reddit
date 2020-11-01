f57d336d18db417ace07f30b170d93ffbe95ff27
virsail <virsaileric@gmail.com>
2020-11-01 17:33:16 +0300

    register the model classes in admin.py

--- amazon/admin.py
+++ amazon/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Projects, Profile
+from .models import Projects, Profile, Review
 # Register your models here.
 
 admin.site.register(Projects)
 admin.site.register(Profile)
+admin.site.register(Review)
 
