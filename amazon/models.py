96ee26351e4d070924f4be0a131c34df16558247
virsail <66640798+Virsail@users.noreply.github.com>
2020-11-02 15:44:29 +0300

    Update models.py

--- amazon/models.py
+++ amazon/models.py
@@ -40,7 +40,6 @@ class Projects(models.Model):
     project_description = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     Owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
-  # owner_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True)
     link = models.URLField()
     
 
